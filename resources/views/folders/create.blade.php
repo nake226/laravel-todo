5e3c4f771396fdec5a5e06874cf923ed59b18eb7
中井 貴之 <t-nakai@mac215.local>
2020-01-17 17:05:35 +0900

    add: エラーメッセージ：フォルダ作成画面

--- resources/views/folders/create.blade.php
+++ resources/views/folders/create.blade.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>ToDo App</title>
-  <link rel="stylesheet" href="/css/styles.css">
+  <link rel="stylesheet" href="/css/style.css">
 </head>
 <body>
   <header>
@@ -20,6 +20,15 @@
           <nav class="panel panel-default">
             <div class="panel-heading">フォルダを追加する</div>
             <div class="panel-body">
+              @if ($errors->any())
+                <div class="alert alert-danger">
+                  <ul>
+                    @foreach($errors->all() as $message)
+                      <li>{{ $message }}</li>
+                    @endforeach
+                  </ul>
+                </div>
+              @endif
               <form action="{{ route('folders.create') }}" method="post">
                 @csrf
                 <div class="form-group">
