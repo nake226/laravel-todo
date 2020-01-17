0a949cca7c6e3a17a1ac513dcf92382bf760c3a4
中井 貴之 <t-nakai@mac215.local>
2020-01-17 15:50:12 +0900

    add: フォルダーとタスクのリレーション

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -18,7 +18,7 @@ class TaskController extends Controller
         $current_folder = Folder::find($id);
 
         // フォルダに紐づくタスクの取得
-        $tasks = Task::where('folder_id', $current_folder->id)->get();
+        $tasks = $current_folder->tasks()->get();
 
         return view('tasks/index', [
             'folders' => $folders,
