a74e03c992af253764d487e70d4d2284f824c92e
中井 貴之 <t-nakai@mac215.local>
2020-01-17 14:45:55 +0900

    add: タスクのステータスのアクセサ

--- app/Task.php
+++ app/Task.php
@@ -6,5 +6,26 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    //
+    /**
+     * 状態定義
+     */
+    const STATUS = [
+        1 => ['label' => '未着手'],
+        2 => ['label' => '着手中'],
+        3 => ['label' => '完了'],
+    ];
+
+    public function getStatusLabelAttribute()
+    {
+        // 状態値
+        $status = $this->attributes['status'];
+
+        // 定義されていなければ空文字を返す
+        if (!isset(self::STATUS[$status])) {
+            return '';
+        }
+
+        // ステータスに応じた文字列を返す
+        return self::STATUS[$status]['label'];
+    }
 }
