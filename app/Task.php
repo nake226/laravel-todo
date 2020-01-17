d12b8b807845e697034000e49be0c5752166c23b
中井 貴之 <t-nakai@mac215.local>
2020-01-17 14:55:03 +0900

    add: タスクの状態に応じたスタイル

--- app/Task.php
+++ app/Task.php
@@ -10,11 +10,15 @@ class Task extends Model
      * 状態定義
      */
     const STATUS = [
-        1 => ['label' => '未着手'],
-        2 => ['label' => '着手中'],
-        3 => ['label' => '完了'],
+        1 => ['label' => '未着手', 'class' => 'label-danger'],
+        2 => ['label' => '着手中', 'class' => 'label-info'],
+        3 => ['label' => '完了', 'class' => ''],
     ];
 
+    /**
+     * 状態のラベルを返す
+     * @return string
+     */
     public function getStatusLabelAttribute()
     {
         // 状態値
@@ -27,5 +31,23 @@ class Task extends Model
 
         // ステータスに応じた文字列を返す
         return self::STATUS[$status]['label'];
+    }
+
+    /**
+     * 状態を表すHTMLクラスを返す
+     * @return string
+     */
+    public function getStatusClassAttribute()
+    {
+        // 状態値
+        $status = $this->attributes['status'];
+
+        // 定義されていなければ空文字を返す
+        if (!isset(self::STATUS[$status])) {
+            return '';
+        }
+
+        // ステータスに応じたクラスを返す
+        return self::STATUS[$status]['class'];
     }
 }
