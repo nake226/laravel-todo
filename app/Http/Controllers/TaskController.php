027d10b7cbb60aa2ad1511692460bd59ff126760
中井 貴之 <t-nakai@mac215.local>
2020-01-17 14:31:48 +0900

    add: タスクデータの引き渡し

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,17 +3,27 @@
 namespace App\Http\Controllers;
 
 use App\Folder;
+use App\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
     public function index(int $id)
     {
+
+        // フォルダーの取得
         $folders = Folder::all();
+
+        // 選択中のフォルダー
+        $current_folder = Folder::find($id);
+
+        // フォルダに紐づくタスクの取得
+        $tasks = Task::where('folder_id', $current_folder->id)->get();
 
         return view('tasks/index', [
             'folders' => $folders,
-            'current_folder_id' => $id,
+            'current_folder_id' => $current_folder->id,
+            'tasks' => $tasks
         ]);
     }
 }
