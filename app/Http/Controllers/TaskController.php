3148450f4fd195d072dc072fc679858a2bc3f3d4
中井 貴之 <t-nakai@mac215.local>
2020-01-20 16:26:01 +0900

    add: タスク編集のアクション

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -57,4 +57,16 @@ class TaskController extends Controller
             'id' => $current_folder->id,
         ]);
     }
+
+    /**
+     * タスクの編集
+     */
+    public function showEditForm(int $task_id)
+    {
+        $task = Task::find($task_id);
+
+        return view('tasks/edit', [
+            'task' => $task,
+        ]);
+    }
 }
