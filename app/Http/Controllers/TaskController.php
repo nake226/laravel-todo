6ad260bf1fcf7e944a50271b4e1560031de82f61
中井 貴之 <t-nakai@mac215.local>
2020-01-20 13:28:35 +0900

    add: タスク作成のアクション

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Folder;
 use App\Task;
-use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
@@ -24,6 +23,35 @@ class TaskController extends Controller
             'folders' => $folders,
             'current_folder_id' => $current_folder->id,
             'tasks' => $tasks
+        ]);
+    }
+
+    /**
+     * タスクの新規作成
+     */
+    public function showCreateForm(int $id)
+    {
+        return view('tasks/create', [
+            'folder_id' => $id
+        ]);
+    }
+
+    /**
+     * タスクの作成、保存をした後にタスク一覧へリダイレクト
+     */
+    public function create(createTask $request)
+    {
+        // タスクのインスタンス作成
+        $task = new Task();
+        // タイトル
+        $task->title = $request->title;
+        // 期限
+        $task->due_date = $request->due_date;
+        // 作成したインスタンスをDBに保存
+        $task->save();
+
+        return redirect()->route('tasks.index', [
+            'id' => $task->folder_id,
         ]);
     }
 }
