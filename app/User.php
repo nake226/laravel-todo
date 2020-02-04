2c12df8f16d32a5fcfabed5b332ed07cb9a05a20
中井 貴之 <t-nakai@mac215.local>
2020-02-04 17:14:28 +0900

    add: ユーザーモデルとフォルダーモデルの関連付け

--- app/User.php
+++ app/User.php
@@ -36,4 +36,13 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * ユーザーとフォルダの関連性
+     */
+    public function folders()
+    {
+        return $this->hasMany(('App\Folder'));
+
+    }
 }
