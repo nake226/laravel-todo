815ba1f7513921957b5c4a69e1850a35bb0ebfaf
中井 貴之 <t-nakai@mac215.local>
2020-02-04 18:09:27 +0900

    add: 認証関連のルート

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,4 @@ Route::post('/folders/{id}/tasks/create', 'TaskController@create');
 Route::get('/folders/{id}/tasks/{task_id}/edit', 'TaskController@showEditForm')->name('tasks.edit');
 Route::post('/folders/{id}/tasks/{task_id}/edit', 'TaskController@edit');
 Route::get('/', 'HomeController@index')->name('home');
+Auth::routes();
