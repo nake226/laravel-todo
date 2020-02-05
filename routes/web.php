a3fc536c73f918726aa4efe69a39906f6d2c4f34
中井 貴之 <t-nakai@mac215.local>
2020-02-05 14:24:31 +0900

    add: ミドルウェアによるログイン判定制御

--- routes/web.php
+++ routes/web.php
@@ -11,12 +11,20 @@
 |
 */
 
-Route::get('/folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
-Route::get('/folders/create', 'FolderController@showCreateForm')->name('folders.create');
-Route::post('/folders/create', 'FolderController@create');
-Route::get('/folders/{id}/tasks/create', 'TaskController@showCreateForm')->name('tasks.create');
-Route::post('/folders/{id}/tasks/create', 'TaskController@create');
-Route::get('/folders/{id}/tasks/{task_id}/edit', 'TaskController@showEditForm')->name('tasks.edit');
-Route::post('/folders/{id}/tasks/{task_id}/edit', 'TaskController@edit');
-Route::get('/', 'HomeController@index')->name('home');
+Route::group(['middleware' => 'auth'], function () {
+  // トップページ
+  Route::get('/', 'HomeController@index')->name('home');
+  // タスク一覧
+  Route::get('/folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
+  // フォルダ作成
+  Route::get('/folders/create', 'FolderController@showCreateForm')->name('folders.create');
+  Route::post('/folders/create', 'FolderController@create');
+  // タスク作成
+  Route::get('/folders/{id}/tasks/create', 'TaskController@showCreateForm')->name('tasks.create');
+  Route::post('/folders/{id}/tasks/create', 'TaskController@create');
+  // タスク編集
+  Route::get('/folders/{id}/tasks/{task_id}/edit', 'TaskController@showEditForm')->name('tasks.edit');
+  Route::post('/folders/{id}/tasks/{task_id}/edit', 'TaskController@edit');
+});
+
 Auth::routes();
