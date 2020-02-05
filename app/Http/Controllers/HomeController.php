8f81ad2a3c5a7a8fbf50905e00bf2c6325dd8789
中井 貴之 <t-nakai@mac215.local>
2020-02-05 14:50:16 +0900

    add: フォルダの有無によるページのリダイレクト処理

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,12 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
     //
     public function index()
     {
-        return view('home');
+        // ログインユーザーの取得
+        $user = Auth::user();
+
+        // ログインユーザーのフォルダを1つ取得
+        $folder = $user->folders()->first();
+
+        // フォルダが無ければ作成を促す
+        if (is_null($folder)) {
+            return view('home');
+        }
+
+        // フォルダがあれば、タスク一覧ページへ
+        return redirect()->route('tasks.index', [
+            'id' => $folder->id
+        ]);
     }
 }
